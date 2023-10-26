b846bd9b38e82c8f44359b78f060232ded18fd3e
Peter Cianelli <p.cianelli@yahoo.com>
2023-10-26 11:56:07 -0500

    implemented metrics publisher and started implementing metrics constants but more constants to implement in the future

--- DailyDoseLambda/src/main/java/com/nashss/se/dailydose/exceptions/MedicationNotFoundException.java
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/exceptions/MedicationNotFoundException.java
@@ -2,7 +2,7 @@ package com.nashss.se.dailydose.exceptions;
 
 public class MedicationNotFoundException extends RuntimeException{
     /**
-     * Exception to throw when a given Medication name and customerId is not found
+     * Exception to throw when a given customerId is not found
      * in the database.
      */
 
--- DailyDoseLambda/src/main/java/com/nashss/se/dailydose/metrics/MetricsConstants.java
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/metrics/MetricsConstants.java
@@ -1,4 +1,11 @@
 package com.nashss.se.dailydose.metrics;
 
 public class MetricsConstants {
+
+    public static final String SERVICE = "Service";
+
+    public static final String SERVICE_NAME = "DailyDose";
+
+    public static final String NAMESPACE_NAME = "U3/DailyDose";
+
 }

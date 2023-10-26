338f902fa6a887d359afc481469800ceddca7a4e
Peter Cianelli <p.cianelli@yahoo.com>
2023-10-26 13:17:56 -0500

    implemented metricsModule, daoModule, and ServiceComponent for the GetMedicationsActivity

--- DailyDoseLambda/src/main/java/com/nashss/se/dailydose/dependency/DaoModule.java
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/dependency/DaoModule.java
@@ -1,4 +1,25 @@
 package com.nashss.se.dailydose.dependency;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.nashss.se.dailydose.dynamodb.DynamoDbClientProvider;
+import dagger.Provides;
+
+import javax.inject.Singleton;
+
+/**
+ * Dagger Module providing dependencies for DAO classes.
+ */
 public class DaoModule {
+
+    /**
+     * Provides a DynamoDBMapper singleton instance.
+     *
+     * @return DynamoDBMapper object
+     */
+    @Provides
+    @Singleton
+    public DynamoDBMapper provideDynamoDBMapper(){
+        return new DynamoDBMapper(DynamoDbClientProvider.getDynamoDBClient(Regions.US_EAST_2));
+    }
 }
--- DailyDoseLambda/src/main/java/com/nashss/se/dailydose/dependency/ServiceComponent.java
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/dependency/ServiceComponent.java
@@ -1,4 +1,20 @@
 package com.nashss.se.dailydose.dependency;
 
+import com.nashss.se.dailydose.activity.GetMedicationsActivity;
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+/**
+ * Dagger component for providing dependency injection in the Music Playlist Service.
+ */
+@Singleton
+@Component(modules = {DaoModule.class, MetricsModule.class})
 public interface ServiceComponent {
+
+    /**
+     * Provides the relevant activity.
+     * @return GetMedicationsActivity
+     */
+    GetMedicationsActivity provideGetMedicationsActivity();
 }

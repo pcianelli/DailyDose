0f933c4d03c60b8fc4756ce45c5573cf1470559d
Peter Cianelli <p.cianelli@yahoo.com>
2023-11-09 13:33:34 -0600

    fixed more checkstyles

--- DailyDoseLambda/src/main/java/com/nashss/se/dailydose/lambda/GetMedicationsLambda.java
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/lambda/GetMedicationsLambda.java
@@ -1,26 +1,26 @@
 package com.nashss.se.dailydose.lambda;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.nashss.se.dailydose.activity.requests.GetMedicationsRequest;
 import com.nashss.se.dailydose.activity.results.GetMedicationsResult;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class GetMedicationsLambda
         extends LambdaActivityRunner<GetMedicationsRequest, GetMedicationsResult>
         implements RequestHandler<AuthenticatedLambdaRequest<GetMedicationsRequest>, LambdaResponse> {
     @Override
     public LambdaResponse handleRequest(AuthenticatedLambdaRequest<GetMedicationsRequest> input, Context context) {
-        return super.runActivity(
-                () -> {
-                    GetMedicationsRequest unauthenticatedRequest = input.fromBody(GetMedicationsRequest.class);
-                    return input.fromUserClaims(claims ->
-                            GetMedicationsRequest.builder()
-                                    .withCustomerId(claims.get("email"))
-                                    .withMedName(unauthenticatedRequest.getMedName())
-                                    .build());
-                },
-                (request, serviceComponent) ->
-                        serviceComponent.provideGetMedicationsActivity().handleRequest(request)
+        return super.runActivity(() -> {
+                GetMedicationsRequest unauthenticatedRequest = input.fromBody(GetMedicationsRequest.class);
+                return input.fromUserClaims(claims ->
+                    GetMedicationsRequest.builder()
+                        .withCustomerId(claims.get("email"))
+                        .withMedName(unauthenticatedRequest.getMedName())
+                            .build());
+        },
+            (request, serviceComponent) ->
+                serviceComponent.provideGetMedicationsActivity().handleRequest(request)
         );
     }
 }

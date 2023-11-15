935a56f9c29a03bec45c9890633fad2bdde67829
Peter Cianelli <p.cianelli@yahoo.com>
2023-11-15 15:59:45 -0600

    created and implemented RemoveMedicationLambda

--- DailyDoseLambda/src/main/java/com/nashss/se/dailydose/lambda/RemoveMedicationLambda.java
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/lambda/RemoveMedicationLambda.java
@@ -1,4 +1,27 @@
 package com.nashss.se.dailydose.lambda;
 
-public class RemoveMedicationLambda {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.dailydose.activity.requests.RemoveMedicationRequest;
+import com.nashss.se.dailydose.activity.results.RemoveMedicationResult;
+
+public class RemoveMedicationLambda
+        extends LambdaActivityRunner<RemoveMedicationRequest, RemoveMedicationResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<RemoveMedicationRequest>, LambdaResponse> {
+
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<RemoveMedicationRequest> input, Context context) {
+        return super.runActivity(
+                () -> {
+                    RemoveMedicationRequest unauthenticatedRequest = input.fromBody(RemoveMedicationRequest.class);
+                    return input.fromUserClaims(claims ->
+                            RemoveMedicationRequest.builder()
+                                    .withCustomerId(claims.get("email"))
+                                    .withMedName(unauthenticatedRequest.getMedName())
+                                    .build());
+                },
+                (request, serviceComponent) ->
+                        serviceComponent.provideRemoveMedicationActivity().handleRequest(request)
+        );
+    }
 }

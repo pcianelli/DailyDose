10053bb8e89ebd5a34a5f0a4563d56b0115737e6
Peter Cianelli <p.cianelli@yahoo.com>
2023-11-17 11:26:09 -0600

    created and implemented updateMedicationInfo.html updated index.js to go first to login if havnt logged in or create account before loading first page

---
+++ web/src/pages/updateMedicationInfo.js
@@ -0,0 +1,12 @@
+import DailyDoseClient from '../api/dailyDoseClient';
+import Header from '../components/header';
+import BindingClass from '../util/bindingClass';
+import DataStore from '../util/DataStore';
+
+/**
+* Logic needed for the update medication info page of the website.
+*/
+class UpdateMedicationInfo extends BindingClass {
+
+
+}

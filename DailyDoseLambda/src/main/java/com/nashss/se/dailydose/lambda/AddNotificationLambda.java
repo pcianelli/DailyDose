396ce46616ea2f227a7aca82f13d6b2c097ad96c
Peter Cianelli <p.cianelli@yahoo.com>
2023-11-20 15:00:10 -0600

    created and implemented AddNotificationLambda, and serviceComponent

---
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/lambda/AddNotificationLambda.java
@@ -0,0 +1,27 @@
+package com.nashss.se.dailydose.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.dailydose.activity.requests.AddNotificationRequest;
+import com.nashss.se.dailydose.activity.results.AddNotificationResult;
+
+public class AddNotificationLambda
+        extends LambdaActivityRunner<AddNotificationRequest, AddNotificationResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<AddNotificationRequest>, LambdaResponse> {
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<AddNotificationRequest> input, Context context) {
+        return super.runActivity(
+            () -> {
+                AddNotificationRequest unauthenticatedRequest = input.fromBody(AddNotificationRequest.class);
+                return input.fromUserClaims(claims ->
+                    AddNotificationRequest.builder()
+                        .withCustomerId(claims.get("email"))
+                        .withMedName(unauthenticatedRequest.getMedName())
+                        .withTime(unauthenticatedRequest.getTime())
+                        .build());
+            },
+            (request, serviceComponent) ->
+                serviceComponent.provideAddNotificationActivity().handleRequest(request)
+        );
+    }
+}

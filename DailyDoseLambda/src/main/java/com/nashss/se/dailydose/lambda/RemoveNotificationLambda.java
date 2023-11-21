87fe10b27404b4f5def7d7a7b370bfe8178d8ba8
Peter Cianelli <p.cianelli@yahoo.com>
2023-11-21 15:36:52 -0600

    created RemoveNotificationLambda

---
+++ DailyDoseLambda/src/main/java/com/nashss/se/dailydose/lambda/RemoveNotificationLambda.java
@@ -0,0 +1,32 @@
+package com.nashss.se.dailydose.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.dailydose.activity.requests.RemoveNotificationRequest;
+import com.nashss.se.dailydose.activity.results.RemoveNotificationResult;
+
+public class RemoveNotificationLambda
+        extends LambdaActivityRunner<RemoveNotificationRequest, RemoveNotificationResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<RemoveNotificationRequest>, LambdaResponse> {
+
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<RemoveNotificationRequest> input, Context context) {
+        return super.runActivity(
+                () -> {
+                    RemoveNotificationRequest unauthenticatedRequest = input.fromPath(path ->
+                            RemoveNotificationRequest.builder()
+                                    .withMedName(path.get("medName"))
+                                    .withTime(path.get("time"))
+                                    .build());
+                    return input.fromUserClaims(claims ->
+                            RemoveNotificationRequest.builder()
+                                    .withCustomerId(claims.get("email"))
+                                    .withMedName(unauthenticatedRequest.getMedName())
+                                    .withTime(unauthenticatedRequest.getTime())
+                                    .build());
+                },
+                (request, serviceComponent) ->
+                        serviceComponent.provideRemoveNotificationActivity().handleRequest(request)
+        );
+    }
+}
